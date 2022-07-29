5776414936639b729d097175cfaddbb8779458ae
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-29 19:03:53 +0530

    [Sharmila] .net core concepts - Generics

---
+++ CoreConcepts/Generics_coreconcepts.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreConcepts
+{
+    public class Generics_coreconcepts
+    {
+        //generic methods
+        public static void display(int[] inputArray)
+        {
+            foreach (var input in inputArray)
+            {
+                Console.WriteLine(input);
+            }
+            Console.WriteLine("------------------------------------");
+        }
+        public static void display(string[] inputArray)
+        {
+            foreach (string input in inputArray)
+            {
+                Console.WriteLine(input);
+            }
+            Console.WriteLine("------------------------------------");
+        }
+        public static void display(double[] inputArray)
+        {
+            foreach (double input in inputArray)
+            {
+                Console.WriteLine(input);
+            }
+            Console.WriteLine("------------------------------------");
+        }
+    }
+    public class Implementgenericmethod
+        {
+        //After generic method implementation
+        public static void print<T>(T[] inputArray)
+        {
+            foreach (var input in inputArray)
+            {
+                Console.WriteLine(input);
+            }
+            Console.WriteLine("------------------------------------");
+        }
+    }
+    // generic concept in class
+    public class GenericClass<T>
+    {
+        private T[] inputArray;
+        // constructor is used
+        public GenericClass(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void toPrint()
+        {
+            foreach (var input in inputArray)
+            {
+                Console.WriteLine(input);
+            }
+            Console.WriteLine("------------------------------------");
+        }
+    }
+}
+
---
+++ CoreConcepts/Program.cs
@@ -0,0 +1,21 @@
+﻿//method generics without generics concepts
+int[] intArray = {1,2,3, 4, 5};
+string[] strArray = { "Apple", "Banana", "Peach", "Cherry", "Mango" };
+double[] douArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+Console.WriteLine("Output before using Generic concept: ");
+CoreConcepts.Generics_coreconcepts.display(intArray);
+CoreConcepts.Generics_coreconcepts.display(strArray);
+CoreConcepts.Generics_coreconcepts.display(douArray);
+
+//method generics with generics concepts
+Console.WriteLine("Output after using Generic concept: ");
+CoreConcepts.Implementgenericmethod.print<int>(intArray);
+CoreConcepts.Implementgenericmethod.print<string>(strArray);
+CoreConcepts.Implementgenericmethod.print<double>(douArray);
+
+
+//Generics Concepts in Class
+Console.WriteLine("Output while using Generic Concept in Class: ");
+new CoreConcepts.GenericClass<int>(intArray).toPrint();
+new CoreConcepts.GenericClass<double>(douArray).toPrint();
+new CoreConcepts.GenericClass<string>(strArray).toPrint();
