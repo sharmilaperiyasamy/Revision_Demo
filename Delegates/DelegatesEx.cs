c15cad4f100b5008ddc50d4b48258d359a628554
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-31 20:28:12 +0530

    [Sharmila] delegates for singlecasting and multicasting

---
+++ Delegates/DelegatesEx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Single Cast Delegates
+
+// for giving static input and single parameter passed inside the function
+delegate int ArithmeticOperation(int num);
+namespace Delegates
+{
+     public class DelegatesEx
+    {
+        static int num = 20;
+        public static int Addition(int p)
+        {
+            num = num + p;
+            return num;
+        }
+        public static int Multiplication(int p)
+        {
+            num = num * p;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿//creating additional one instance for multicast)
+ArithmeticOperation arithOp;
+
+//create delegate instances(single instance)
+ArithmeticOperation add = new ArithmeticOperation(Delegates.DelegatesEx.Addition);
+ArithmeticOperation mul = new ArithmeticOperation(Delegates.DelegatesEx.Multiplication);
+
+//calling the methods using the delegate objects
+add(10);
+Console.WriteLine("Output of Addition : {0}", Delegates.DelegatesEx.getNum());
+mul(5);
+Console.WriteLine("Output of Multiplication : {0}", Delegates.DelegatesEx.getNum());
+Console.ReadKey();
+
+
+// for multicasting delegate comment the single cast methods calling
+arithOp = add;
+arithOp = arithOp + mul;
+//calling multicast
+arithOp(5);
+Console.WriteLine("Output for Arithmetic Operation : {0}", Delegates.DelegatesEx.getNum());
+Console.ReadKey();
