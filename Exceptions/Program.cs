f61062c904d9b02fbbc78034a01acd69be0b2dfa
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-08-01 14:45:02 +0530

    [Sharmila] Adding exception and its types

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,6 +1,6 @@
-﻿Exceptions.ExceptionClass.indexexp();
-Exceptions.ExceptionClass.Nullexp();
-Exceptions.ExceptionClass.Withdraw(500);
-Exceptions.ExceptionClass.DivideByTwo(3);
-Exceptions.ExceptionClass.Argumentnullexp();
-Exceptions.ExceptionClass.argumentoutofrangeexp();
+﻿Exceptionstypes.ExceptionClass.indexexp();
+Exceptionstypes.ExceptionClass.Nullexp();
+Exceptionstypes.ExceptionClass.Withdraw(500);
+Exceptionstypes.ExceptionClass.DivideByTwo(3);
+Exceptionstypes.ExceptionClass.Argumentnullexp();
+Exceptionstypes.ExceptionClass.argumentoutofrangeexp();
